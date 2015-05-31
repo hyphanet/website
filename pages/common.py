b8ad2f301982ff05b99e81330a6899f23ccc8072
Gerard Krol <gerard@gerardkrol.nl>
2015-05-31 20:18:17 +0200

    Add favicon

--- pages/common.py
+++ pages/common.py
@@ -40,6 +40,12 @@ def head(title):
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 <meta name="description" content="" />
 <meta name="author" content="" />
+
+<!-- IE -->
+<link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico" />
+<!-- other browsers -->
+<link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+
 <!--[if IE]>
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <![endif]-->
