66200e106c9e972ae9a3f2ae93409904162f3eb4
Steve Dougherty <steve@asksteved.com>
2015-10-03 14:29:06 -0400

    Move "What is Freenet?" to the About page

--- pages/about.py
+++ pages/about.py
@@ -4,6 +4,60 @@ import string
 import markdown
 from .common import *
 
+
+class IntroductionSection(Section):
+    def __init__(self):
+        self.slug = "introduction"
+        self.title = _("What is Freenet?")
+    def get_content(self):
+        # License: GFDL (from old freenetproject.org website)
+        content = _("""
+> _"I worry about my child and the Internet all the time, even though she's too
+>  young to have logged on yet. Here's what I worry about. I worry that 10 or
+>  15 years from now, she will come to me and say 'Daddy, where were you when
+>  they took freedom of the press away from the Internet?'"_
+> --Mike Godwin, [Electronic Frontier Foundation](https://www.eff.org/)
+
+Freenet is free software which lets you anonymously share files, browse and
+publish "freesites" (web sites accessible only through Freenet) and chat on
+forums, without fear of censorship. Freenet is decentralised to make it less
+vulnerable to attack, and if used in "darknet" mode, where users only connect to
+their friends, is very difficult to detect.
+
+Communications by Freenet nodes are encrypted and are routed through other
+nodes to make it extremely difficult to determine who is requesting the
+information and what its content is.
+
+Users contribute to the network by giving bandwidth and a portion of their
+hard drive (called the "data store") for storing files. Files are
+automatically kept or deleted depending on how popular they are, with the
+least popular being discarded to make way for newer or more popular content.
+Files are encrypted, so generally the user cannot easily discover what is in
+his datastore, and hopefully can't be held accountable for it. Chat forums,
+websites, and search functionality, are all built on top of this distributed
+data store.
+
+Freenet has been downloaded over 2 million times since the project started,
+and used for the distribution of censored information all over the world
+including countries such as China and the Middle East. Ideas and concepts
+pioneered in Freenet have had a significant impact in the academic world. Our
+2000 paper "Freenet: A Distributed Anonymous Information Storage and
+Retrieval System" was the most cited computer science paper of 2000 according
+to Citeseer, and Freenet has also inspired papers in the worlds of law and
+philosophy. Ian Clarke, Freenet's creator and project coordinator,
+was selected as one of the top 100 innovators of 2003 by MIT's Technology
+Review magazine.
+
+An important recent development, which very few other networks have, is the
+"darknet": By only connecting to people they trust, users can greatly reduce
+their vulnerability, and yet still connect to a global network through their
+friends' friends' friends and so on. This enables people to use Freenet even
+in places where Freenet may be illegal, makes it very difficult for
+governments to block it, and does not rely on tunneling to the "free world".
+""")
+        return text(md(content))
+
+
 class PhilosophySection(Section):
     def __init__(self):
         self.slug = "philosopy"
@@ -213,5 +267,10 @@ class AboutPage(Page):
     def __init__(self):
         self.slug = "about"
         self.title = _("About")
-        self.sections = [PhilosophySection(), PeopleSection(), PapersSection()]
+        self.sections = [
+            IntroductionSection(),
+            PhilosophySection(),
+            PeopleSection(),
+            PapersSection()
+        ]
         self.first_section_in_menu = True
