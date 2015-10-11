c278fe091b3b74c2beff6666c08a335afa959ef9
Steve Dougherty <steve@asksteved.com>
2015-10-11 18:59:24 -0400

    Add translator comment support and comment

--- update_pot.sh
+++ update_pot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-find -name "*.py" | xargs xgettext --from-code=UTF-8 -o - | ./clean_po.py > freenet_site.pot
+find -name "*.py" | xargs xgettext --add-comments=Translators: --from-code=UTF-8 -o - | ./clean_po.py > freenet_site.pot
 # now msgmerge with itself to keep changes in the translated files to a minimum later
 msgmerge -U freenet_site.pot freenet_site.pot
