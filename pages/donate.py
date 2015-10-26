4e5e382956572b85664e3216781be9982faac867
Bert Massop <bert.massop@gmail.com>
2015-10-26 21:48:55 +0100

    Create a separate translation entry for each sponsor
    This simplifies future translation of sponsor text, that we expect to
    add to in the future.
    
    Since we're changing translations anyways, this also creates a new line
    for every single sentence. This might assist the Translation Memory in
    matching, and makes translating the sentences slightly easier.

--- pages/donate.py
+++ pages/donate.py
@@ -185,34 +185,55 @@ class SponsorsSection(Section):
         return text(md(_("""
 The following persons, organisations and companies have generously sponsored
 this project through donations or discounts on hardware and services:
-
+""") + "\n\n" + _("""
 *   [John Pozadzides][url_john_pozadzides]  
-     John is the founder of HTMLHelp.com and former Vice President of Sales for SAVVIS Communications. John has donated $10,000 to the Freenet Project to fund ongoing development of the Freenet software.
+John is the founder of HTMLHelp.com and former Vice President of Sales for SAVVIS Communications.
+John has donated $10,000 to the Freenet Project to fund ongoing development of the Freenet software.
+""") + "\n" + _("""
 *   [Web Hosting Search][url_web_hosting_search]  
-     WebHostingSearch is a top rated guide to various world wide web hosting companies. Mr Chris Reynolds and the friendly team behind this company have become one of our proud promoters.
+WebHostingSearch is a top rated guide to various world wide web hosting companies.
+Mr Chris Reynolds and the friendly team behind this company have become one of our proud promoters.
+""") + "\n" + _("""
 *   [John Gilmore][url_john_gilmore]  
-     John Gilmore is one of the founders of the Electronic Frontier Foundation, the Cypherpunks mailing list, and Cygnus Solutions. He created the alt.* hierarchy in Usenet and is a major contributor to the GNU project. He has donated $10,000.
+John Gilmore is one of the founders of the Electronic Frontier Foundation, the Cypherpunks mailing list, and Cygnus Solutions.
+He created the alt.* hierarchy in Usenet and is a major contributor to the GNU project.
+He has donated $10,000.
+""") + "\n" + _("""
 *   [Bytemark Hosting][url_bytemark_hosting]  
-     Bytemark provides discounted hosting facilities to Freenet and comes with our highest recommendation.
+Bytemark provides discounted hosting facilities to Freenet and comes with our highest recommendation.
+""") + "\n" + _("""
 *   [zoozle Torrent Search][url_zoozle]  
-     zoozle is a search engine for BitTorrent and P2P networks. It is also available in [german][url_zoozle_german] and [french][url_zoozle_french]. They have donated $1,500.
+zoozle is a search engine for BitTorrent and P2P networks.
+It is also available in [german][url_zoozle_german] and [french][url_zoozle_french].
+They have donated $1,500.
+""") + "\n" + _("""
 *   [YourKit, LLC][url_yourkit]  
-     YourKit is kindly supporting Freenet by providing us a free of charge license of its full-featured Java Profiler. YourKit, LLC is the creator of innovative and intelligent tools for profiling Java applications. You can learn more about [YourKit Java Profiler][url_yourkit_profiler] following the link.
+YourKit is kindly supporting Freenet by providing us a free of charge license of its full-featured Java Profiler.
+YourKit, LLC is the creator of innovative and intelligent tools for profiling Java applications.
+You can learn more about [YourKit Java Profiler][url_yourkit_profiler] following the link.
+""") + "\n" + _("""
 *   [Google][url_google]  
-     Google [open source][url_google_opensource] have three times donated $18,000, as well as paying for students to work with us over summer since 2006 through the [Google Summer of Code][url_google_soc] program.
+Google [open source][url_google_opensource] have three times donated $18,000, as well as paying for students to work with us over summer since 2006 through the [Google Summer of Code][url_google_soc] program.
+""") + "\n" + _("""
 *   [Finanzvergleich][url_finanzvergleich]  
-     Finanzvergleich is a German financial services comparison site, who have contributed $1,500.
+Finanzvergleich is a German financial services comparison site, who have contributed $1,500.
+""") + "\n" + _("""
 *   [TopDir][url_topdir]  
-     TopDir is a human edited web directory with informations about interesting topics and websites in the German language.
+TopDir is a human edited web directory with informations about interesting topics and websites in the German language.
+""") + "\n" + _("""
 *   [All Filters][url_allfilters] - another generous sponsor.
+""") + "\n" + _("""
 *   [A German Site for Gardening and Plants][url_gartenheinz] - another generous sponsor.
+""") + "\n" + _("""
 *   [Simple Car Rentals][url_simple_carrentals]  
-     Simple Car Rentals has generously supported Freenet by helping us with some of our administrative expenses over the years.
+Simple Car Rentals has generously supported Freenet by helping us with some of our administrative expenses over the years.
+""") + "\n" + _("""
 *   [The Sports Den][url_sportsden]  
-     The Sports Den has a wide variety of outdoor sports equipment, they are based in Ireland but can ship to the UK, Europe, and the United States.
+The Sports Den has a wide variety of outdoor sports equipment, they are based in Ireland but can ship to the UK, Europe, and the United States.
+""") + "\n" + _("""
 *   ![][logo_intellij_idea]  
-    [JetBrains][url_jetbrains] has kindly provided a license for the Ultimate edition of [IntelliJ IDEA][url_intellij_idea].
-
+[JetBrains][url_jetbrains] has kindly provided a license for the Ultimate edition of [IntelliJ IDEA][url_intellij_idea].
+""") + "\n\n" + _("""
 Freenet Project Inc does not necessarily endorse the business activities of all of the companies listed on this page.
 
 Donation inquiries please contact Ian Clarke: {donate_email}
