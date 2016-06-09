7845f66d4b57c8ab540732c4d739da75ea694a46
Arne Babenhauserheide <bab@draketo.de>
2016-06-10 00:01:44 +0200

    contribute: add bug tracker to main text and link FCP docs

--- pages/contribute.py
+++ pages/contribute.py
@@ -14,9 +14,13 @@ If you already have a contribution in mind you can find the project repositories
 
 - Get in contact in the [mailing lists](help.html#mailing-lists)
 - Join us on IRC: [#freenet on chat.freenode.net](help.html#irc)
+- Report issues in the [bug tracker](https://bugs.freenetproject.org/)
 
-A list of freenet-related projects is available
-[in the wiki](https://wiki.freenetproject.org/Projects).
+The Wiki contains a list of 
+[freenet-related projects](https://wiki.freenetproject.org/Projects) 
+and documentation of the 
+[Freenet Client Protocol](https://wiki.freenetproject.org/FCPv2)
+for using Freenet from external programs.
 
 We're happy to accept pull requests on GitHub as well as patches sent under a
 pseudonym through Freenet forums such as [FMS](http://freesocial.draketo.de/fms_en.html).
