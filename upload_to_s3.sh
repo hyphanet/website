23a93bf103565dab42beb5a28f63f8fe6fdebf19
Florent Daigniere <nextgens@freenetproject.org>
2017-03-18 10:05:37 +0100

    the regexp is wrong and we shouldn't delete files

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -11,6 +11,6 @@ source ~/virtualenv/python2.7/bin/activate
 BUCKET="$1"
 CACHE_TIME="$2"
 
-aws s3 sync --exclude '*\.(bz2|gz|aac|flac|mp3|wma|gif|jpg|jpeg|png|avi|mov|mp4|webm|woff|woff2)$' --exclude='.webassets-cache' --metadata-directive  REPLACE --content-encoding=gzip --region=us-east-1 --cache-control "public, max-age=$CACHE_TIME" --delete output/ $BUCKET
+aws s3 sync --exclude '*' --include '(?!*\.(bz2|gz|aac|flac|mp3|wma|gif|jpg|jpeg|png|avi|mov|mp4|webm|woff|woff2)$)' --exclude='.webassets-cache' --metadata-directive  REPLACE --content-encoding=gzip --region=us-east-1 --cache-control "public, max-age=$CACHE_TIME" output/ $BUCKET
 # gzip_cache won't deal with these
-aws s3 sync --include '*\.(bz2|gz|aac|flac|mp3|wma|gif|jpg|jpeg|png|avi|mov|mp4|webm|woff|woff2)$' --exclude='.webassets-cache' --metadata-directive  REPLACE --content-encoding='' --region=us-east-1 --cache-control "public, max-age=$CACHE_TIME" --delete output/ $BUCKET
+aws s3 sync --exclude '*' --include '*\.(bz2|gz|aac|flac|mp3|wma|gif|jpg|jpeg|png|avi|mov|mp4|webm|woff|woff2)$' --exclude='.webassets-cache' --metadata-directive  REPLACE --content-encoding='' --region=us-east-1 --cache-control "public, max-age=$CACHE_TIME" output/ $BUCKET
