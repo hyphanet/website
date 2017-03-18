8e931f49e553657bf48a07193c5e042a2504ed82
Florent Daigniere <nextgens@freenetproject.org>
2017-03-18 10:15:29 +0100

    it still doesn't work

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -11,6 +11,7 @@ source ~/virtualenv/python2.7/bin/activate
 BUCKET="$1"
 CACHE_TIME="$2"
 
-aws s3 sync --exclude '*' --include '(?!*\.(bz2|gz|aac|flac|mp3|wma|gif|jpg|jpeg|png|avi|mov|mp4|webm|woff|woff2)$)' --exclude='.webassets-cache' --metadata-directive  REPLACE --content-encoding=gzip --region=us-east-1 --cache-control "public, max-age=$CACHE_TIME" output/ $BUCKET
 # gzip_cache won't deal with these
-aws s3 sync --exclude '*' --include '*\.(bz2|gz|aac|flac|mp3|wma|gif|jpg|jpeg|png|avi|mov|mp4|webm|woff|woff2)$' --exclude='.webassets-cache' --metadata-directive  REPLACE --content-encoding='' --region=us-east-1 --cache-control "public, max-age=$CACHE_TIME" output/ $BUCKET
+aws s3 sync --exclude '*' --include '*\.(bz2|gz|aac|flac|mp3|wma|gif|jpg|jpeg|png|avi|mov|mp4|webm|woff|woff2)$' --exclude='.webassets-cache' --region=us-east-1 --cache-control "public, max-age=$CACHE_TIME" output/ $BUCKET
+# we don't specify --metadata-directive  REPLACE so it might just work
+aws s3 sync --exclude='.webassets-cache' --content-encoding=gzip --region=us-east-1 --cache-control "public, max-age=$CACHE_TIME" output/ $BUCKET
