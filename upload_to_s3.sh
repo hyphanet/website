fdbcbe2d27818007d43a943f6f00a0e6bacda384
Florent Daigniere <nextgens@freenetproject.org>
2017-03-18 09:55:41 +0100

    reduce caching to 5mins for dev

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -9,7 +9,8 @@ fi
 source ~/virtualenv/python2.7/bin/activate
 
 BUCKET="$1"
+CACHE_TIME="$2"
 
-aws s3 sync --exclude '*\.(bz2|gz|aac|flac|mp3|wma|gif|jpg|jpeg|png|avi|mov|mp4|webm|woff|woff2)$' --exclude='.webassets-cache' --metadata-directive  REPLACE --content-encoding=gzip --region=us-east-1 --cache-control "public, max-age=86400" --delete output/ $BUCKET
+aws s3 sync --exclude '*\.(bz2|gz|aac|flac|mp3|wma|gif|jpg|jpeg|png|avi|mov|mp4|webm|woff|woff2)$' --exclude='.webassets-cache' --metadata-directive  REPLACE --content-encoding=gzip --region=us-east-1 --cache-control "public, max-age=$CACHE_TIME" --delete output/ $BUCKET
 # gzip_cache won't deal with these
-aws s3 sync --include '*\.(bz2|gz|aac|flac|mp3|wma|gif|jpg|jpeg|png|avi|mov|mp4|webm|woff|woff2)$' --exclude='.webassets-cache' --metadata-directive  REPLACE --content-encoding='' --region=us-east-1 --cache-control "public, max-age=86400" --delete output/ $BUCKET
+aws s3 sync --include '*\.(bz2|gz|aac|flac|mp3|wma|gif|jpg|jpeg|png|avi|mov|mp4|webm|woff|woff2)$' --exclude='.webassets-cache' --metadata-directive  REPLACE --content-encoding='' --region=us-east-1 --cache-control "public, max-age=$CACHE_TIME" --delete output/ $BUCKET
