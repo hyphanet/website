ccff9b5a8f6907eaff8114b3b1ef1be3c2500739
Florent Daigniere <nextgens@freenetproject.org>
2017-03-18 09:40:34 +0100

    hmm?

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -6,6 +6,8 @@ then
 	exit 0
 fi
 
+source ~/virtualenv/python2.7/bin/activate
+
 BUCKET="$1"
 
 aws s3 sync --exclude '*\.(bz2|gz|aac|flac|mp3|wma|gif|jpg|jpeg|png|avi|mov|mp4|webm|woff|woff2)$' --exclude='.webassets-cache' --metadata-directive  REPLACE --content-encoding=gzip --region=us-east-1 --cache-control "public, max-age=86400" --delete output/ $BUCKET
