f509bb5cb14c37becf785509b67948fc53cf1986
Steve Dougherty <steve@asksteved.com>
2023-06-29 00:47:43 -0400

    Change site URL to hyphanet.org

--- publishconf.py
+++ publishconf.py
@@ -7,7 +7,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://freenetproject.org'
+SITEURL = 'https://hyphanet.org'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
