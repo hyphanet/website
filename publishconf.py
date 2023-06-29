b23522ad828dd464eddc74db7f1a2bf2bd0e9385
Steve Dougherty <steve@asksteved.com>
2023-06-29 07:16:52 -0400

    Use consistent canonical url
    Should match CNAME

--- publishconf.py
+++ publishconf.py
@@ -7,7 +7,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://hyphanet.org'
+SITEURL = 'https://www.hyphanet.org'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
